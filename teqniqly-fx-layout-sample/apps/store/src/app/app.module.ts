21e4451fd38a06a3b0f89a6c8c44b2fe32b7db88
farooqam <farooq_am@hotmail.com>
2020-01-10 18:01:29 -0800

    Sample flex layout component

--- teqniqly-fx-layout-sample/apps/store/src/app/app.module.ts
+++ teqniqly-fx-layout-sample/apps/store/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { StoreUiModule } from '@teqniqly-fx-layout-sample/store-ui';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    StoreUiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- teqniqly-fx-layout-sample/libs/store-ui/src/lib/store-ui.module.ts
+++ teqniqly-fx-layout-sample/libs/store-ui/src/lib/store-ui.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule, FlexLayoutModule],
+  declarations: [SampleComponent],
+  exports: [SampleComponent]
 })
 export class StoreUiModule {}
