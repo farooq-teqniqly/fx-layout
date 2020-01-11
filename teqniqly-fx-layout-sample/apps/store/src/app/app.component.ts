e64f002b69acdeed547ce86f22e30f1842652964
farooqam <farooq_am@hotmail.com>
2020-01-10 16:35:13 -0800

    Nx app setup

---
+++ teqniqly-fx-layout-sample/apps/store/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'teqniqly-fx-layout-sample-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'store';
+}
